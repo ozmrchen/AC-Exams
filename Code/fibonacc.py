65a5a5f9342fd150bb8a01f8f2f6dad90005fc74
ozmrchen <119475897+ozmrchen@users.noreply.github.com>
2024-11-08 10:53:53 +1100

    Create fibonacc.py

---
+++ Code/fibonacc.py
@@ -0,0 +1,14 @@
+"""
+ALGORITHM fibonacciWhileLoop()
+BEGIN
+    fib ← 13
+    x ← 0
+    y ← 1
+    WHILE x <= fib DO
+	    PRINT x
+        temp ← x
+        x ← y
+        y ← temp + y
+    ENDWHILE
+END
+"""
